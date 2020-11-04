ef3e3b073cab15fc62130f39f540fff56e908af0
Artur Mądrzak <amadrzak@onplick.com>
2020-11-04 17:39:26 +0100

    Relax retries count for send

--- mtp/mtp.c
+++ mtp/mtp.c
@@ -286,8 +286,8 @@ static void MtpTask(void *handle)
                         break;
                     }
 
-                    int retries = 5;
-                    uint32_t timeout_ms = 5;
+                    int retries = 30;
+                    uint32_t timeout_ms = 1;
                     while((send_status = ScheduleSend(mtpApp, response, result_len)) == kStatus_USB_Busy
                             && --retries
                             && xMessageBufferIsEmpty(mtpApp->outputBox)
