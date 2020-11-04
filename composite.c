e275036ce15952bc718810b4cbb5e0e5eb4163fb
Roman Kubiak <roman.kubiak@mudita.com>
2020-11-04 13:15:41 +0100

    usbInit returns void*

--- composite.c
+++ composite.c
@@ -188,7 +188,7 @@ static usb_status_t USB_DeviceCallback(usb_device_handle handle, uint32_t event,
 }
 
 
-int composite_init(void)
+usb_cdc_vcom_struct_t *composite_init(void)
 {
     if (USB_DeviceClockInit() != kStatus_USB_Success) {
         LOG_ERROR("[Composite] USB Device Clock init failed");
@@ -203,7 +203,7 @@ int composite_init(void)
         USB_DeviceClassInit(CONTROLLER_ID, &g_UsbDeviceCompositeConfigList, &composite.deviceHandle))
     {
         LOG_ERROR("[Composite] USB Device init failed");
-        return -1;
+        return NULL;
     }
     else
     {
@@ -224,6 +224,6 @@ int composite_init(void)
     }
 
     LOG_DEBUG("[Composite] USB initialized");
-    return 0;
+    return &composite.cdcVcom;
 
 }
